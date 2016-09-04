11fe5b904dbbb04fd2a2d94d5ac565f46d1ca93a
demoleas21 <andrew.demoleas@gmail.com>
2016-09-04 00:25:04 -0400

    Added POST functionality

--- src/api.py
+++ src/api.py
@@ -1,5 +1,6 @@
-from flask import Flask, flash, redirect, jsonify, session, url_for
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_pyfile('_config.py')
@@ -8,7 +9,7 @@ db = SQLAlchemy(app)
 from src.models import Comment
 
 
-@app.route('/', methods=['GET'])
+@app.route('/comments', methods=['GET'])
 def get_comments():
     results = db.session.query(Comment).limit(10).offset(0).all()
     json_results = []
@@ -20,9 +21,31 @@ def get_comments():
             'posted_date': result.posted_date
         }
         json_results.append(data)
-    return jsonify(items=json_results)
+    return jsonify(comments=json_results)
+
+
+@app.route('/comments/<int:comment_id>', methods=['GET'])
+def get_individual_comment(comment_id):
+    results = db.session.query(Comment).filter_by(comment_id=comment_id)
+    json_results = []
+    for result in results:
+        data = {
+            'comment_id': result.comment_id,
+            'comment': result.comment,
+            'author': result.author,
+            'posted_date': result.posted_date
+        }
+        json_results.append(data)
+    return jsonify(comments=json_results)
 
 
 @app.route('/post', methods=['POST'])
 def post_comments():
-    pass
+    new_comments = Comment(
+        request.json['comment'],
+        request.json['author'],
+        datetime.utcnow()
+    )
+    db.session.add(new_comments)
+    db.session.commit()
+    return 'POST Successful'

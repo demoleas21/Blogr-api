1d06832b945cbe1ecc77da40acc7a050e74997bc
demoleas21 <andrew.demoleas@gmail.com>
2016-09-03 20:20:36 -0400

    Added GET method to api

--- src/api.py
+++ src/api.py
@@ -1,18 +1,28 @@
-from flask import flash, redirect, jsonify, session, url_for
-from src import app, db
+from flask import Flask, flash, redirect, jsonify, session, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_pyfile('_config.py')
+db = SQLAlchemy(app)
+
 from src.models import Comment
 
 
-@app.route('/')
-def api_comments():
+@app.route('/', methods=['GET'])
+def get_comments():
     results = db.session.query(Comment).limit(10).offset(0).all()
     json_results = []
     for result in results:
         data = {
             'comment_id': result.comment_id,
-            'comment': result.coment,
+            'comment': result.comment,
             'author': result.author,
             'posted_date': result.posted_date
         }
         json_results.append(data)
     return jsonify(items=json_results)
+
+
+@app.route('/post', methods=['POST'])
+def post_comments():
+    pass
